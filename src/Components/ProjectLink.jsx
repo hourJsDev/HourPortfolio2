7d5e3c32a16d1832560ccd2cb537c66cfc8f352c
hourJsDev <145030272+hourJsDev@users.noreply.github.com>
2023-09-27 21:06:00 +0700

    change at projectLink

--- src/Components/ProjectLink.jsx
+++ src/Components/ProjectLink.jsx
@@ -4,7 +4,7 @@ const ProjectLink = ({ link }) => {
     <a
       className="absolute z-50 hover:gap-[2rem] duration-200 transition-all ease-out flex -translate-x-[50%] -translate-y-[50%] bg-white gap-[1rem] px-[1rem] py-[.5rem] text-[1.2rem] rounded-[2rem] items-center top-[50%] left-[50%]"
       href={link}
-      target="_blank"
+      
     >
       View Project{" "}
       <span>
