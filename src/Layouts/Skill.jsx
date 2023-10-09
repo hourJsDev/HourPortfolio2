d731be532c5d865c93d5825d26185c34d83fdc83
hourJsDev <145030272+hourJsDev@users.noreply.github.com>
2023-10-09 16:29:15 +0700

    restyle to animationCard

--- src/Layouts/Skill.jsx
+++ src/Layouts/Skill.jsx
@@ -12,11 +12,11 @@ const Skill = () => {
     
   },[selectedSkill])
   return (
-   <section id="about" className="w-[80%] pt-[4rem] mx-auto pb-[10rem]">
-    <h1 data-aos="fade-up"
-    data-aos-anchor-placement="top-bottom" 
-     data-aos-offset="400"
-     data-aos-duration="700" className="text-[3rem] font-semibold ">My Skills</h1>
+   <section data-aos="fade-up"
+   data-aos-anchor-placement="top-bottom" 
+    data-aos-offset="400"
+    data-aos-duration="500"  id="about" className="w-[80%] pt-[4rem] mx-auto pb-[10rem]">
+    <h1 className="text-[3rem] font-semibold ">My Skills</h1>
     <div className="flex max-md:flex-col max-md:gap-[2rem]  mt-[2rem] ">
         <div className="w-[50%] max-md:w-full flex max-md:pt-[3rem]  items-center justify-center">
           <SkillCard title={title} image={image}/>
